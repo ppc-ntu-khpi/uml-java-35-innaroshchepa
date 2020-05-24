3c2e1f53e067d5916aa671a83336aff5e2d7b62c
innaroshchepa <62202381+innaroshchepa@users.noreply.github.com>
2020-05-24 12:36:32 +0300

    Add files via upload

---
+++ src/distanseLearning/Mark.java
@@ -0,0 +1,15 @@
+package distanseLearning;
+
+public class Mark {
+
+    private Integer id;
+
+    private Integer studentId;
+
+    private Integer lessonsId;
+
+    private Integer Mark;
+
+    public Mark(Integer studentId, Integer lessonsId) {
+    }
+}
